02cca7060471296a6cd9255fddf39bc264fcb986
parkjinuk09 <parkjinuk@gmail.com>
2026-02-09 13:49:58 +0900

    signup 완료

---
+++ src/main/java/com/example/jichini/JichiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.jichini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JichiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JichiniApplication.class, args);
+    }
+
+}
